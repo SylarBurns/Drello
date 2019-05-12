dd195bb8e8e0c5713cee5afe288c46645b6ab307
HyeWon Jeon <hyewon@HyeWonui-MacBook-Pro.local>
2019-05-13 03:56:10 +0900

    fix typo

--- init_board.py
+++ init_board.py
@@ -36,6 +36,7 @@ board = """CREATE TABLE Board (
 boardMember= """CREATE TABLE BoardMember(
     Board_ID int NOT NULL,
     User_ID varchar(32) NOT NULL,
+    Permission varchar(16) NOT NULL,
     CONSTRAINT board_member UNIQUE (Board_ID, User_ID),
     FOREIGN KEY (Board_ID) REFERENCES Board(Board_ID) ON UPDATE CASCADE
 )"""
